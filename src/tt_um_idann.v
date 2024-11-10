d3613f363dfe162eaa94795d4c510bde99aa78b0
Arian Heidari <arianheidari@Arians-MBP.attlocal.net>
2024-11-09 17:34:50 -0800

    on_bp test 2

--- src/tt_um_idann.v
+++ src/tt_um_idann.v
@@ -41,7 +41,7 @@ state_mach state_mach_inst (.clk_i(clk), .rst_i(rst_n), .en_i(1), .init_i(ui_in[
 
 hidden_neuron hn0 (.clk_i(clk), .rst_i(rst_n), .en_i(f0p_o | f1p_o), .x_i(ui_in[3:0]), .w0_i(1), .w1_i(2), .w2_i(3), .w3_i(4), .hidden_neuron_o(hn0_o));
 
-hidden_neuron hn1 (.clk_i(clk), .rst_i(rst_n), .en_i(f0p_o | f1p_o), .x_i(ui_in[3:0]), .w0_i(1), .w1_i(2), .w2_i(3), .w3_i(4), .hidden_neuron_o(hn1_o));
+hidden_neuron hn1 (.clk_i(clk), .rst_i(rst_n), .en_i(f0p_o | f1p_o), .x_i(ui_in[3:0]), .w0_i(5), .w1_i(6), .w2_i(7), .w3_i(4), .hidden_neuron_o(hn1_o));
 
 // hidden_neuron hn2 (.clk_i(clk), .rst_i(rst_n), .en_i(f0p_o | f1p_o), .x_i(ui_in[3:0]), .w0_i(1), .w1_i(2), .w2_i(3), .w3_i(4), .hidden_neuron_o(hn2_o));
 
@@ -60,11 +60,11 @@ output_neuron on0(.clk_i(clk), .rst_i(rst_n), .en_i(f0p_o | f1p_o), .zero_loss_i
 
 //backprop hidden-to-out weight0
 wire [7:0] w0_on_i, w0_on_backprop_o;
-assign w0_on_i = (f0p_o) ? 1 : w0_on_backprop_o; 
+assign w0_on_i = (f0p_o) ? 5 : w0_on_backprop_o; 
 output_backprop obp1 (.clk_i(clk), .en_i(bp_o), .rst_i(rst_n), .final_i(final_o), .x_i(ui_in[3:0]), .hidden_val_i(hn0_o), .w_i(on_weights_o[7:0]), .zero_weight_reset_i(zero_weight_update_o), .w_o(w0_on_backprop_o), .b_end_o(b_end0_o));
 
 wire [7:0] w1_on_i, w1_on_backprop_o;
-assign w1_on_i = (f0p_o) ? 1 : w1_on_backprop_o; 
+assign w1_on_i = (f0p_o) ? 7 : w1_on_backprop_o; 
 output_backprop obp2 (.clk_i(clk), .en_i(bp_o), .rst_i(rst_n), .final_i(final_o), .x_i(ui_in[3:0]), .hidden_val_i(hn1_o), .w_i(on_weights_o[15:8]), .zero_weight_reset_i(zero_weight_update_o), .w_o(w1_on_backprop_o), .b_end_o(b_end1_o));
 
 
